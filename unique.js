80b92971a0df541c8737c6f1f645fde27d6c3b61
DexZed <144045080+DexZed@users.noreply.github.com>
2024-08-16 16:42:11 +0600

    "Updated HTML, CSS, and JavaScript files to use unique IDs and class selectors for multiple slideshows, and refactored JavaScript code to support multiple slideshows."

--- unique.js
+++ unique.js
@@ -1,14 +1,18 @@
 $(window).on('load', function() {
+    $('.slideshow').each(function() {
+        var $slideshow = $(this); // Store a reference to the current slideshow
 
-  $("#slideshow > div:gt(0)").hide();
+        // Hide all slides except the first one within this slideshow
+        $slideshow.children('div:gt(0)').hide();
 
-  
-  setInterval(function() { 
-    $('#slideshow > div:first')
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo('#slideshow');
-  }, 3000);
+        // Set an interval for the slideshow animation
+        setInterval(function() {
+            $slideshow.children('div:first')
+                .fadeOut(1000)
+                .next()
+                .fadeIn(1000)
+                .end()
+                .appendTo($slideshow);
+        }, 3000);
+    });
 });
